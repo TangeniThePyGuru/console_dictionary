c4a7634f0ce831650236ad666cf246d14f93cca3
Tangeni Shikomba <tangenishikomba@gmail.com>
2017-03-13 02:27:51 +0200

    Server currently working smoothly!

--- Distributed_Systems_Assignment/src/classes/ServerThread.java
+++ Distributed_Systems_Assignment/src/classes/ServerThread.java
@@ -4,85 +4,86 @@ import java.io.IOException;
 import java.io.PrintStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.sql.SQLException;
 import java.util.Scanner;
 
 //Thread Class to support threading
 
-public class ServerThread extends Thread {
+public class ServerThread extends Thread{
+//	capture the port for the server
+	public static String port = "", 
+//			store the definition
+			temp, 
+//			store the word to search for
+			word,
+//			store the ip addresses of the server
+			serverIP = "";
+	
+	public static TextFile dictionary = new TextFile();
 	
 	
-	public void run() {
-		System.out.println("-- Server up and running --");
-		/*		declaration */
-				
-//				store to search for
-				String word;
-//				store the definition
-				String temp = null;
-//				connect to DataBase
-				DBConnect connect = new DBConnect();
-				
+	public void run() {    
+    	/*		declaration */
 		
-//				connect to client via the port 1000
+		ServerSocket serveSocket = null;
+		try {
+			serveSocket = new ServerSocket(Integer.parseInt(port));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+			
 		
-				ServerSocket serveSocket = null;
-				try {
-					serveSocket = new ServerSocket(1000);
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				
-				
-				while(true){
-//					accept connection with client
-					Socket clientSocket = null;
-					try {
-						clientSocket = serveSocket.accept();
-					} catch (IOException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-//					get client input e.g word to find
-					Scanner clientInput = null;
-					try {
-						clientInput = new Scanner(clientSocket.getInputStream());
-					} catch (IOException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-					word = clientInput.next();
-					
-//					get the definition of the word from the database
-					try {
-						temp = connect.getDefinition(word);
-					} catch (SQLException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-//					send the definition to the client
-					PrintStream p = null;
-					try {
-						p = new PrintStream(clientSocket.getOutputStream());
-					} catch (IOException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-					p.println(temp);
-
-//					close connection
-//					serveSocket.close();
-					
-//					close clientInput scanner
-					clientInput.close();
-					p.close();
-					clientSocket.isClosed();
-				}
+		System.out.println("-- Server up and running on port "+port+" --");
+		while(true){
+//			accept connection with client
+			Socket clientSocket = null;
+			try {
+				clientSocket = serveSocket.accept();
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
 			}
+			
+//			get client input e.g word to find
+			Scanner clientInput = null;
+			try {
+				clientInput = new Scanner(clientSocket.getInputStream());
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			word = clientInput.next();
+			
+//			read the definition from the text file/ dictionary
+			temp = dictionary.definition(word);
+			
+//			send the definition to the client
+			PrintStream p = null;
+			try {
+				p = new PrintStream(clientSocket.getOutputStream());
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			p.println(temp);
+			
+//			close clientInput scanner
+			clientInput.close();
+			p.close();
+			try {
+				clientSocket.close();
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		}
+	}
 
+	
     public static void main(String args[]) {
-        (new ServerThread()).start();
+        port = args[0];
+       (new ServerThread()).start();
+        
     }
-
+    
 }
